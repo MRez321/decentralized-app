3d284aa493d277875ae3c06b1e28917863871116
Reza <MRez321@gmail.com>
2024-05-30 17:34:57 +0330

    20240530 17:35

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 
+
 const port = process.env.PORT || 3000;
 const app = express();
 const store = new MongoDBStore({
@@ -50,10 +51,15 @@ app.use((req, res, next) => {
 
     User.findById(req.session.user._id)
     .then(user => {
+        if (!user) {
+            return next();
+        }
         req.user = user;
         next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+        throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
@@ -69,6 +75,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
